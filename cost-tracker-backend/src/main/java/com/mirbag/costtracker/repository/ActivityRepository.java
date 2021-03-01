8f7e2c31167d46f1102cc852d2f90fab298a5cd4
pretest735 <mdiqbalhafiz735@gmail.com>
2021-03-01 23:13:52 +0600

    Added Activity and User controller and repository.

--- cost-tracker-backend/src/main/java/com/mirbag/costtracker/repository/ActivityRepository.java
+++ cost-tracker-backend/src/main/java/com/mirbag/costtracker/repository/ActivityRepository.java
@@ -3,6 +3,8 @@ package com.mirbag.costtracker.repository;
 import com.mirbag.costtracker.entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
+import java.util.List;
 
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    List<Activity> findByIsActiveTrue();
 }
